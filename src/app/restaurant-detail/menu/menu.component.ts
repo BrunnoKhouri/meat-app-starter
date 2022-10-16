63b4d82e90534d399d456ebcdc26d31a56b6a403
Brunno Khouri <brunno.k.d.borges@gmail.com>
2022-10-16 17:37:49 -0300

    ADD-CURRENCY

--- src/app/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -24,7 +24,4 @@ export class MenuComponent implements OnInit {
     this.menu = this.restaurantsService
       .menuOfRestaurant(this.route.parent.snapshot.params['id']);
   }
-  // addMenuItem(item: MenuItem) {
-  //   console.log('item', item);
-  // }
 }
